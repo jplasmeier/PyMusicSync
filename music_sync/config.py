1aa44c4bce236bfd061156c6449d481a616461aa
Justin Plasmeier <jplasmeier@gmail.com>
2016-11-29 20:06:59 -0500

    actually adds config and logging

---
+++ music_sync/config.py
@@ -0,0 +1,13 @@
+# config.py
+# Configuration module for Mass Media Sync.
+
+"""
+Config JSON Schema:
+
+google_drive_secret (?)
+google_drive_folder
+
+usb_device_path
+usb_device_sn (from IOREG)
+
+"""
---
+++ music_sync/logger.py
@@ -0,0 +1,6 @@
+# logger.py
+# Logging module for Mass Media Sync
+
+
+def log_warning(warning_message):
+    print 'Warning: ', warning_message
